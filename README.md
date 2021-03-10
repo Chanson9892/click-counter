b65fdd6e5f5baed9a5bc153b6a9eb982a9bdb0b8
Chanson9892 <chan.hanson5@gmail.com>
2021-03-09 18:39:07 -0800

    added a changing linear gradient based on click counter

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Readme for my basic click counter app
+## Click Counter
+
+each click of the increase button moves the gradient to the right.
+each click of the decrease button moves the gradient to the left unless the count is 0.
+reset button resets the counter and the gradient.
--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 var clicks = 0
+var backgroundPercent = 10
 var increaseButton = document.getElementById('btn-increase')
 var decreaseButton = document.getElementById('btn-decrease')
 var resetButton = document.getElementById('btn-reset')
 
 increaseButton.onclick = function(){
     clicks++
-    document.getElementById("clicks").innerHTML = clicks;
-    console.log(clicks)
+    backgroundPercent += 10
+    document.getElementById("clicks").innerHTML = clicks
+    setGradient()
 }
+
 decreaseButton.onclick = function(){
     if (clicks > 0){
         clicks--
-        document.getElementById("clicks").innerHTML = clicks;
-        console.log(clicks)
+        backgroundPercent -= 10
+        document.getElementById("clicks").innerHTML = clicks
+        setGradient()
     }
 }
 
 resetButton.onclick = function(){
     clicks = 0
-    document.getElementById("clicks").innerHTML = clicks;
-    console.log(clicks)
+    backgroundPercent = 10
+    document.getElementById("clicks").innerHTML = clicks
+    setGradient()
+}
+
+function setGradient() {
+    document.body.style.background = "linear-gradient(to right, rgb(56, 185, 228), white " + backgroundPercent + "%, lightblue 30%, green)";
 }
 
